f13d39b8e631c993ec69d1b9e70ecccb6181800d
djboburjon <djboburdev@gmail.com>
2023-09-23 21:29:39 +0500

    project added

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+const navBtn = document.querySelector('#menu');
+const menuBar = document.querySelector('[role="menubar"]');
+
+navBtn.addEventListener('click', () => {
+  const isExpanded = JSON.parse(navBtn.getAttribute('aria-expanded'));
+  navBtn.setAttribute('aria-expanded', !isExpanded);
+  menuBar.classList.toggle('hidden');
+  menuBar.classList.toggle('flex');
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
